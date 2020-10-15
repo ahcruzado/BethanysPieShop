efbb95158337e18bfbf8a1c5c2dbe4b1e75339a0
Alejandro Cruzado <ahcruzado@users.noreply.github.com>
2020-10-15 11:04:25 -0300

    Initial commit

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            this.pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = this.pieRepository.GetPiesOfTheWeek()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
