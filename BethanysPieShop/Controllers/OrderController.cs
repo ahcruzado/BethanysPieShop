f79d764103c741953c9e1225143e6edc41262a01
Alejandro Cruzado <ahcruzado@users.noreply.github.com>
2020-10-16 22:00:44 -0300

    Módulo08 completo

---
+++ BethanysPieShop/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = this.shoppingCart.GetShoppingCartItems();
+            shoppingCart.ShoppingCartItems = items;
+
+            if(shoppingCart.ShoppingCartItems.Count==0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderRepository.CreateOrder(order);
+                shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {                    
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our decicious pies!";
+            return View();
+        }
+    }
+}
